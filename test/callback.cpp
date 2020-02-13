96ab17446fbc2b5934b41c73f0bf18340f800e5a
Daniël Emmery <daniel.emmery@protonmail.com>
2020-02-13 22:05:06 +0100

    Test cases for lazy callback

--- test/callback.cpp
+++ test/callback.cpp
@@ -3,15 +3,15 @@
 
 struct callback_cmd
 {
-    int count;
+    int count = 1;
     std::string name;
     std::string final_name;
 
     template<class F>
     void parse(F f)
     {
-        auto cb = [this](auto& c, const auto&, const args::argument& arg) {
-            if (arg.count > 0) for(int i=0;i<c;i++) final_name += name;
+        auto cb = [this](auto& c, const auto&, const args::argument&) {
+            for(int i=0;i<c;i++) final_name += name;
         };
         f(count, "--count", "-C", args::callback(cb));
         f(name, "--name", "-N");
@@ -34,9 +34,86 @@ PROVE_CASE()
 {
     callback_cmd cmd{};
     args::parse(cmd, {"--name", "hello"});
-    PROVE_CHECK(cmd.count == 0);
+    PROVE_CHECK(cmd.count == 1);
     PROVE_CHECK(cmd.name == "hello");
-    PROVE_CHECK(cmd.final_name == "");
+    PROVE_CHECK(cmd.final_name == "hello");
+}
+
+struct lazy_callback_cmd
+{
+    int count = 1;
+    std::string name;
+    std::string final_name;
+
+    template<class F>
+    void parse(F f)
+    {
+        auto cb = [this](auto& c, const auto&, const args::argument&) {
+            for(int i=0;i<c;i++) final_name += name;
+        };
+        f(count, "--count", "-C", args::lazy_callback(cb));
+        f(name, "--name", "-N");
+    }
+
+    void run()
+    {}
+};
+
+PROVE_CASE()
+{
+    lazy_callback_cmd cmd;
+    args::parse(cmd, {"--count", "5", "--name", "hello"});
+    PROVE_CHECK(cmd.count == 5);
+    PROVE_CHECK(cmd.name == "hello");
+    PROVE_CHECK(cmd.final_name == "hellohellohellohellohello");
+}
+
+PROVE_CASE()
+{
+    lazy_callback_cmd cmd{};
+    args::parse(cmd, {"--name", "hello"});
+    PROVE_CHECK(cmd.count == 1);
+    PROVE_CHECK(cmd.name == "hello");
+    PROVE_CHECK(cmd.final_name.empty());
+}
+
+struct lazy_callback_null_cmd
+{
+    int count = 0;
+    std::string name;
+    std::string final_out;
+
+    template<class F>
+    void parse(F f)
+    {
+        auto cb = [this](auto&&, const auto&, const args::argument&) {
+            final_out = "hello";
+        };
+        f(nullptr, "--out", "-O", args::lazy_callback(cb));
+        f(count, "--count", "-C");
+        f(name, "--name", "-N");
+    }
+
+    void run()
+    {}
+};
+
+PROVE_CASE()
+{
+    lazy_callback_null_cmd cmd;
+    args::parse(cmd, {"--out", "--count", "5", "--name", "hello"});
+    PROVE_CHECK(cmd.count == 5);
+    PROVE_CHECK(cmd.name == "hello");
+    PROVE_CHECK(cmd.final_out == "hello");
+}
+
+PROVE_CASE()
+{
+    lazy_callback_null_cmd cmd;
+    args::parse(cmd, {"--count", "5", "--name", "hello"});
+    PROVE_CHECK(cmd.count == 5);
+    PROVE_CHECK(cmd.name == "hello");
+    PROVE_CHECK(cmd.final_out.empty());
 }
 
 struct eager_callback_cmd
